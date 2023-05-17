820246c9da5a4641f1cdd46a4d9dcae33cdb8c4a
william arasirwa <mageboarasirwa03@gmail.com>
2023-05-17 12:00:29 +0300

    complete

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,30 @@
 # Write your code here
+
+require 'pry'
+
+def reverse_each_word(sentence)
+    # split
+
+    words = sentence.split
+
+    # initializa a new array
+
+    reversed_words=[]
+
+    # loop over words
+
+    words.each do |word|
+
+        reversed_words << word.reverse
+    end
+
+    # join words together
+
+    reversed_words.join("")
+
+
+end
+
+binding.pry
+
+reverse_each_word("Hello there, and how are you?")
